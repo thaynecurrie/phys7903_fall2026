9859cca3ed93dfb1e6e2ffb2dbfb8f03c17928af
thaynecurrie <thayne.currie@gmail.com>
2026-08-24 14:01:00 -0500

    updated Basics notes

--- ScientificPythonNotes/Basic/code_ipynb/ex2.py
+++ ScientificPythonNotes/Basic/code_ipynb/ex2.py
@@ -5,7 +5,8 @@ def calc_fwhm(d_tel=8.2,wvlh_microns=[1.25,1.65,2.18]):
  
  fwhm=1.028*206265*np.array(wvlh_microns)*1e-6/d_tel
 
- print(fwhm)
+ print('the fwhm is ',fwhm)
+ return fwhm
 
 
 if __name__ == '__main__':
